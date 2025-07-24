ca519a493b95a684631c84aceb0b4251bb0fb522
Ayman Elh <elhamioui1964@gmail.com>
2025-07-24 16:39:10 +0100

    fix an issue about docker with adding docker compose file and install tailwincss 4 to use in styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import Header from "./components/common/Header.jsx";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
